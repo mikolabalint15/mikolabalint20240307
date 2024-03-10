3bad502858a451ff3ed8761c8e564f2c238cbd09
mikolabalint15 <mb04mb@gmail.com>
2024-03-10 17:27:24 +0100

    Submitted Task 2024.03.10
    Elkészítettem az általad kért feladatot, remélem megfelel az elvárásaidnak :D

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,39 +9,45 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class UserComponent implements OnInit {
 
-  userData: any;
+  userData!: User;
 
-  constructor(
-    private route: ActivatedRoute,
-    private http: HttpClient
-  ) { }
+  constructor(private route: ActivatedRoute,private http: HttpClient) {}
 
-  id: string = "-1";
 
   ngOnInit(): void {
-    this.id = this.route.snapshot.paramMap.get('id')!
-    //alert(this.id)
-    this.getUser(this.id)
+    this.getUser();
   }
 
-  getUser(id: string){
-    let url = "https://jsonplaceholder.typicode.com/users/" + id;
-    this.http.get<any>(url).subscribe(
-      res=>{
-        this.userData = res;
-      }
-    )
+  getUser() {
+    this.http.get<User>('https://jsonplaceholder.typicode.com/users/1')
+      .subscribe((data: User) => {
+        this.userData = data;
+      });
   }
 
 }
 
 //Megcsinálni a https://jsonplaceholder.typicode.com/users/1 responsa alapján
 type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
   address: {
     street: string,
+    suite: string;
+    city: string;
+    zipcode: string;
     geo: {
-      lat: number,
-      lng: number
+      lat: string,
+      lng: string
     }
   } 
+  phone: string;
+  website: string;
+  company:{
+    name: string,
+    catchPhrase:string,
+    bs: string
+  }
 }
